81ab533b5bec95393a1704eef363cec6803922f7
linoor <michal.pomaranski@gmail.com>
2012-11-04 10:32:54 +0100

    liczniki i przycisk
    Dodaję dwa liczniki i przycisk na górze ekranu.

--- kod/gen/io/saper/R.java
+++ kod/gen/io/saper/R.java
@@ -15,7 +15,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int TableLayout=0x7f070000;
+        public static final int imageButton1=0x7f070003;
+        public static final int licznik_min=0x7f070004;
+        public static final int menu_settings=0x7f070005;
+        public static final int tableRow1=0x7f070001;
+        public static final int timer=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -27,7 +32,9 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int timer=0x7f040004;
         public static final int title_activity_main=0x7f040003;
+        public static final int wartosc_poczatkowa_licznikow=0x7f040005;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
