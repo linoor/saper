9493069658fc4a49cf16ad19896e5f845ca80af0
linoor <michal.pomaranski@gmail.com>
2012-11-04 10:40:39 +0100

    zmienianie czcionki
    Zmieniam czcionki liczników.

--- kod/src/io/saper/MainActivity.java
+++ kod/src/io/saper/MainActivity.java
@@ -2,14 +2,24 @@ package io.saper;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+private TextView timer, minecount;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        /*tworzenie napisów(liczników)*/
+        timer = (TextView) findViewById(R.id.timer);
+        timer = (TextView) findViewById(R.id.licznik_min);
+        /*zmienianie czcionki*/
+        Typeface lcd_font = Typeface.createFromAsset(getAssets(), "fonts/LCDPHONE.ttf");
+        timer.setTypeface(lcd_font);
+        minecount.setTypeface(lcd_font);
     }
 
     @Override
