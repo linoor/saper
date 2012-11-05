b7d975614250b929387359b913bfc8e4c5f03688
linoor <michal.pomaranski@gmail.com>
2012-11-05 09:57:50 +0100

    zmiany przy licznikach
    Zmniejszyłem czcionki, zmieniłem kolor.

--- kod/gen/io/saper/R.java
+++ kod/gen/io/saper/R.java
@@ -10,24 +10,28 @@ package io.saper;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int kolor_licznikow=0x7f050000;
+    }
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int ic_action_search=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int smiley=0x7f020003;
     }
     public static final class id {
-        public static final int TableLayout=0x7f070000;
-        public static final int imageButton1=0x7f070003;
-        public static final int licznik_min=0x7f070004;
-        public static final int menu_settings=0x7f070005;
-        public static final int tableRow1=0x7f070001;
-        public static final int timer=0x7f070002;
+        public static final int TableLayout=0x7f080000;
+        public static final int imageButton1=0x7f080003;
+        public static final int licznik_min=0x7f080004;
+        public static final int menu_settings=0x7f080005;
+        public static final int tableRow1=0x7f080001;
+        public static final int timer=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -37,6 +41,6 @@ public final class R {
         public static final int wartosc_poczatkowa_licznikow=0x7f040004;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
