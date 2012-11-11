210bd1950a4a5e88dfd48afcec7b7a9fd142a76f
linoor <michal.pomaranski@gmail.com>
2012-11-11 08:54:01 +0100

    Zmieniłem ikonkę :)

--- kod/gen/io/saper/R.java
+++ kod/gen/io/saper/R.java
@@ -15,12 +15,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int smutna=0x7f020003;
-        public static final int stres=0x7f020004;
-        public static final int usmiech=0x7f020005;
-        public static final int wygrana=0x7f020006;
+        public static final int bomba=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int smutna=0x7f020004;
+        public static final int stres=0x7f020005;
+        public static final int usmiech=0x7f020006;
+        public static final int wygrana=0x7f020007;
     }
     public static final class id {
         public static final int TableLayout=0x7f080000;
