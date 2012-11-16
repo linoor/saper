633c4219bb400252062b2a29bd3226b60bb15bd5
linoor <michal.pomaranski@gmail.com>
2012-11-16 22:36:29 +0100

    Próbuję stworzyć pole minowe

--- kod/src/io/saper/MainActivity.java
+++ kod/src/io/saper/MainActivity.java
@@ -7,11 +7,16 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 private TextView timer, minecount;
 private ImageButton smiley;
+private Block blocks[][];
+private TableLayout pole_minowe;
+private int numer_of_rows = 9;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +25,17 @@ private ImageButton smiley;
         /*tworzenie napisów(liczników)*/
         timer = (TextView) findViewById(R.id.timer);
         minecount = (TextView) findViewById(R.id.licznik_min);
+        
         /*tworzenie referencji do przycisku*/
         smiley = (ImageButton) findViewById(R.id.imageButton1);
+        
         /*zmienianie czcionki licznikow*/
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/LCDM2B__.TTF");
         timer.setTypeface(font);
         minecount.setTypeface(font);
+        
+        /*ustawianie pola minowego*/
+        pole_minowe = (TableLayout) findViewById(R.id.pole_minowe);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +43,8 @@ private ImageButton smiley;
         return true;
     }
     
-    
+    private void showMineField()
+    {
+    	
+    }
 }
