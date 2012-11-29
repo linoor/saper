e9167682d171432e0f7b5631697f51a399752d5f
linoor <michal.pomaranski@gmail.com>
2012-11-29 09:33:47 +0100

    Poprawiłem trochę ten projekt testowy

---
+++ SaperTestTest/src/io/saper/test/ProbnyTest.java
@@ -0,0 +1,20 @@
+package io.saper.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import io.saper.MainActivity;
+import junit.framework.TestCase;
+
+public class ProbnyTest extends ActivityInstrumentationTestCase2<MainActivity>
+{
+
+	public ProbnyTest(String s)
+	{
+		super(s, null);
+	}
+
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+	}
+
+}
