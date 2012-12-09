b0f6eb06986b3f425e329276d515782150ab36db
linoor <michal.pomaranski@gmail.com>
2012-12-09 09:38:05 +0100

    Dodaję test sprawdzajączy czy działą funkcją stopTimer()

---
+++ SaperTestTest/src/io/saper/test/stopTimer.java
@@ -0,0 +1,54 @@
+package io.saper.test;
+
+import java.util.Random;
+
+import junit.framework.Assert;
+
+import com.jayway.android.robotium.solo.Solo;
+
+import io.saper.Block;
+import io.saper.MainActivity;
+import io.saper.R;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class stopTimer extends ActivityInstrumentationTestCase2<MainActivity>
+{
+
+	private Solo solo;
+	
+	public stopTimer()
+	{
+		super("io.saper", MainActivity.class);
+	}
+
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
+	public void testShortClick()
+	{
+		Random rand = new Random();
+		for(int i = 0; i < 3; i++)
+		{
+			Block block = (Block) solo.getButton(rand.nextInt(9*9));
+			solo.clickOnView(block);
+		}
+		getActivity().stopTimer();
+			synchronized(solo)
+			{
+				String s = solo.getView(R.id.timer).toString();
+				try
+				{
+					solo.wait(1000);
+				} catch (InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+				String s2 = solo.getView(R.id.timer).toString();
+				Assert.assertEquals(s, s2);
+			}
+			
+		}
+	}
