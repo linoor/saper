a00f2c757fb48057e6bbc60064641cc8edd126a1
cosmia <angela.czubak@gmail.com>
2013-01-14 01:53:04 +0100

    test zegara

---
+++ SaperTestTest/src/io/saper/structtest/ZegarTest.java
@@ -0,0 +1,55 @@
+package io.saper.structtest;
+
+import java.util.Random;
+
+import junit.framework.Assert;
+
+import com.jayway.android.robotium.solo.Solo;
+
+import io.saper.Block;
+import io.saper.DaneGraczy;
+import io.saper.Gra;
+import io.saper.MainActivity;
+import io.saper.MineField;
+import io.saper.Plansza;
+import io.saper.R;
+import io.saper.Zegar;
+//import io.saper.R;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ImageButton;
+
+public class ZegarTest extends ActivityInstrumentationTestCase2<MainActivity>
+{
+
+	private Solo solo;
+	
+	public ZegarTest()
+	{
+		super("io.saper", MainActivity.class);
+	}
+
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
+	public void testMineField()
+	{
+		solo.enterText(0, "jakasnazwa");
+		solo.clickOnButton(0);
+		Zegar z = Zegar.getInstance();
+		z.setTimerstarted(true);
+		assertEquals(true, z.isTimerstarted());
+		z.setTimerstarted(false);
+		assertEquals(false, z.isTimerstarted());
+		z.setTimerstarted(true);
+		assertEquals(true, z.isTimerstarted());
+		z.setTimerstarted(false);
+		assertEquals(false, z.isTimerstarted());
+		z.setCzas(878);
+		assertEquals(878, z.getCzas());
+		z.setCzas(43);
+		assertEquals(43, z.getCzas());
+	}
+}
